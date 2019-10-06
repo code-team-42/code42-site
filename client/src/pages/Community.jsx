ad8702fed1cc9715a8f85576001929d964d9a77a
ashan <ashannon1990@gmail.com>
2019-10-06 17:10:42 -0400

    added community page and event component

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -5,15 +5,13 @@ const Community = () => {
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Events!</h1>
-      <div style={{ flex: 'across' }}>
-        <Event
-          eventImage="https://via.placeholder.com/350x150"
-          name="Example Event!!"
-          info="The event has an address, date, and time. Go to it at the location."
-          link="/"
-          linkText="Click here"
-        />
-      </div>
+      <Event
+        eventImage="https://via.placeholder.com/350x150"
+        name="Example Event!!"
+        info="The event has an address, date, and time. Go to it at the location."
+        link="/"
+        linkText="Click here"
+      />
     </div>
   )
 }
